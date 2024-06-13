728beaf6b775b0347e586f59edffa27cf041fc43
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-06-13 13:00:06 +0100

    make array version of dad jokes

---
+++ src/jokesData.jsx
@@ -0,0 +1,22 @@
+export default [
+  {
+    setup: "Why did the scarecrow win an award?",
+    punchline: "Because he was outstanding in his field!",
+  },
+  {
+    setup: "What do you call a lazy kangaroo?",
+    punchline: "Pouch potato.",
+  },
+  {
+    setup: "Did you hear about the restaurant on the moon?",
+    punchline: "Great food, no atmosphere.",
+  },
+  {
+    setup: "What do you call a fish with no eyes?",
+    punchline: "Fsh!",
+  },
+  {
+    setup: "Why did the bicycle fall over?",
+    punchline: " Because it was two tired!",
+  },
+];
